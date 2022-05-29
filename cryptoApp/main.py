718d8cb2fce5bf908c4d847c1eeba7c7f220794b
LostPy <lostmusic.osu@gmail.com>
2022-05-29 11:59:19 +0200

    restructure the project and add base files for ui

---
+++ cryptoApp/main.py
@@ -0,0 +1,10 @@
+"""Main file for CryptoApp, it's the entry point.
+"""
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+
+app = QApplication(sys.argv)
+# here create the window
+app.exec()
--- tests/test_db_init.py
+++ tests/test_db_init.py
@@ -1,9 +1,9 @@
 import os.path
 import sys
-sys.path.append(os.path.abspath("../poo-crypto-common"))
+sys.path.append(os.path.abspath("../poo-crypto-common/cryptoApp"))
 print(sys.path)
 
-from crypto_common.db import CryptoDatabase
+from crypto_core.db import CryptoDatabase
 
 
 db = CryptoDatabase(CryptoDatabase.create_connection("test"))
