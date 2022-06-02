3c6a2d54439772da48fa1cdcbf94a79d54786b13
LostPy <lostmusic.osu@gmail.com>
2022-06-02 18:53:41 +0200

    Some custom widgets for ui: + Add a CryptoWidget to use in a QListWidget, this widget display some data from the cryptocurrencies and the amount stock in portfolio + Add a PortfolioChart class, a subclass of QChartView to display a pie chart on the repartitions of cryptocurrencies of portfolio (in value)

---
+++ cryptoApp/crypto_ui/widgets/portfolioChart.py
@@ -0,0 +1,35 @@
+from PySide6.QtCharts import QChartView, QChart, QPieSeries, QPieSlice
+from PySide6.QtGui import QPainter
+
+from crypto_core.objects import Portofolio
+
+
+class PortfolioChart(QChartView):
+
+    def __init__(self, portfolio: Portofolio, /,
+                 title: str = "",
+                 theme=QChart.ChartThemeLight,
+                 parent=None):
+        super().__init__(parent)
+        self.portfolio = portfolio
+        self.init_chart()
+        self.chart().setTitle(title)
+        self.chart().setTheme(theme)
+
+    def init_chart(self):
+        chart = QChart()
+        serie = QPieSeries()
+        serie.append([
+            QPieSlice(currency.name, amount * currency.price)
+            for currency, amount in self.portfolio.currencies.items()
+        ])  # Create slices with currency's name as label and total value as value
+
+        for pieslice in serie.slices():
+            # must be executed after add all slices to compute the good percent value
+            pieslice.setLabel(f"{pieslice.label()} {pieslice.percentage():.2%}")
+        serie.setLabelsPosition(QPieSlice.LabelOutside)
+        serie.setLabelsVisible(True)
+        chart.addSeries(serie)
+        chart.legend().hide()
+        self.setChart(chart)
+        self.setRenderHint(QPainter.Antialiasing)  # to don't have pixelized curves
